2d1dd6c3edbdb360a8ddd7fb6f0f36015484ee46
Nurul-GC <nuruldecarvalho@gmail.com>
2021-10-29 05:25:58 +0200

    update gcrypter/tester.py

--- gcrypter/tester.py
+++ gcrypter/tester.py
@@ -10,10 +10,10 @@ from gcrypter import db
 
 class MyTestCase(unittest.TestCase):
     def test_atualizarcreated(self):
-        self.assertTrue(db.G6RDB().atualizar_created(_nome='Nurul', _created=datetime.today()))
+        self.assertTrue(db.G6RDB().insert_user(_nome='Nurul', _created=datetime.today()))
 
     def test_atualizarlastlogin(self):
-        self.assertTrue(db.G6RDB().atualizar_lastlogin(_nome='Nurul', _last_login=datetime.today()))
+        self.assertTrue(db.G6RDB().update_lastlogin(_nome='Nurul', _last_login=datetime.today()))
 
 
 if __name__ == '__main__':
