3bdd9c385e22c9e100265639956cc353381b3f8c
Nurul-GC <nuruldecarvalho@gmail.com>
2022-01-03 15:25:00 +0200

    update g6r/*

--- g6r/gfuns.py
+++ g6r/gfuns.py
@@ -4,27 +4,7 @@
 from datetime import datetime
 from os import name, path
 from re import compile
-from secrets import token_bytes
 from subprocess import getoutput
-
-
-def encrypt(p: str):
-    """funcao encriptadora"""
-    encriptar = p.encode()
-    encriptar_ = token_bytes(len(p))
-
-    encriptado = int.from_bytes(encriptar, 'big')
-    encriptado_ = int.from_bytes(encriptar_, 'big')
-
-    encriptado_final = encriptado ^ encriptado_
-    return encriptado_final, encriptado_
-
-
-def decrypt(p: int, q: int):
-    """funcao desencriptadora"""
-    palavra_encriptada = int(p) ^ int(q)
-    desencriptada = palavra_encriptada.to_bytes((palavra_encriptada.bit_length() + 7) // 8, 'big')
-    return desencriptada.decode()
 
 
 def debugpath() -> str:
@@ -42,14 +22,14 @@ def perfilnome(_folder: str) -> str or None:
 
 
 def created(_username: str):
-    from gcrypter.db import G6RDB
+    from g6r.db import G6RDB
     db = G6RDB()
     date_created = datetime.today()
     return db.insert_user(_nome=_username, _created=date_created)
 
 
 def logged(_username: str):
-    from gcrypter.db import G6RDB
+    from g6r.db import G6RDB
     db = G6RDB()
     date_last_login = datetime.today()
     return db.update_lastlogin(_nome=_username, _last_login=date_last_login)
